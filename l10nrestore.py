175690e10c365ac041e603bee16ede7426e3a9eb
Tommi Nieminen <software@legisign.org>
2018-10-20 15:53:07 +0300

    Mostly small changes to l10nrestore and modo.

--- l10nrestore.py
+++ l10nrestore.py
@@ -3,6 +3,7 @@
 
   2016-09-13  0.9.91  Added support for .qm files and renamed the program
                       to l10nrestore. (TN)
+  2018-02-20  0.9.92  Getting rid of separate init(). (TN)
 
 '''
 
@@ -12,15 +13,17 @@ import getopt
 import glob
 
 # Constants
-version = '0.9.91'
+version = '0.9.92'
 short_opts = 'l:s:v'
 long_opts = ('l10ndir=',                # -l
              'steps=',                  # -s
              'verbose',                 # -v
              'help')
 
-class ProgramError(Exception):
-    pass
+# Global variables
+l10ndir = '/usr/share/locale/fi/LC_MESSAGES'
+steps = 1
+verbose = False
 
 def warn(msg):
     print('{}: {}'.format(os.path.basename(sys.argv[0]), msg), file=sys.stderr)
@@ -30,89 +33,87 @@ def die(msg):
     sys.exit(1)
 
 def usage():
-    print('''l10nrestore -- palauta kotoistustiedosto varmuuskopiosta
+    global l10ndir, steps
+    print('''{prg} -- palauta kotoistustiedosto varmuuskopiosta
 
 Käyttö:
+    {prg} [VALITSIMET] TIEDOSTO…
 
-    l10nrestore [VALITSIMET] TIEDOSTO…
-    
-Palauttaa kotoistustiedostot (GNU gettextin .mo tai Qt:n .qm) 
-varmuuskopioistaan.
+Palauttaa kotoistustiedostot (.mo, .qm) varmuuskopioistaan.
 
-Valitsimet:
+Valitsimet (suluissa oletukset):
+    -l KANSIO, --l10ndir=KANSIO     kansio, josta tiedostoja etsitään
+    -s N, --steps=N                 palautusaskelten määrä
+    --help                          näytä tämä ohje ja lopeta
 
-    -l KANSIO  --l10ndir=KANSIO
-        kansio, josta tiedostoja etsitään (oletus nykykansio)
-    
-    -s N  --steps=N     
-        palautusaskelten määrä
-    
-    --help              
-        näytä tämä ohje ja lopeta.
-''')
+Oletukset:
+    --l10ndir={l10n}
+    --steps={steps}
+'''.format(prg=os.path.basename(sys.argv[0]), l10n=l10ndir, steps=steps))
 
-def l10n_restore(l10ndir, l10nfile, steps=1, verbose=False):
-    backups = glob.glob(os.path.join(l10ndir, l10nfile + '.~[0-9]*~'))
+def restore(pathname, steps=1, verbose=False):
+    dirname, filename = os.path.split(pathname)
+    backups = glob.glob(os.path.join(dirname, filename + '.~[0-9]*~'))
     if len(backups) < steps:
-        raise ProgramError
+        raise ValueError(filename, len(backups))
     # Any of the following operations may raise an exception
     if verbose:
-        print('{l10n} -> {l10n}.l10nrestore'.format(l10n=l10nfile))
-    os.rename(l10nfile, l10nfile + '.l10nrestore')
+        print('{name} -> {name}.restore'.format(name=filename))
+    os.rename(pathname, os.path.join(dirname, filename + '.restore'))
     if verbose:
-        print('{l10n}.~1~ -> {l10n}'.format(l10n=l10nfile))
-    os.rename(l10nfile + '.~1~', l10nfile)
+        print('{name}.~1~ -> {name}'.format(name=filename))
+    os.rename(filename + '.~1~', filename)
     for gen in range(2, len(backups)):
         if verbose:
-            print('{l10n}.~{old}~ -> {l10n}.~{new}~'.format(l10n=l10nfile,
+            print('{name}.~{old}~ -> {name}.~{new}~'.format(name=filename,
                                                             old=gen,
                                                             new=gen - 1))
-        os.rename(l10nfile + '.~{}~'.format(gen),
-                  l10nfile + '.~{}~'.format(gen - 1))
+        os.rename(filename + '.~{}~'.format(gen),
+                  filename + '.~{}~'.format(gen - 1))
     if verbose:
-        print('{}.~{}~ poistetaan'.format(l10nfile, len(backups)))
-    os.remove('{}.~{}~'.format(l10nfile, len(backups)))
+        print('{}.~{}~ poistetaan'.format(filename, len(backups)))
+    os.remove('{}.~{}~'.format(filename, len(backups)))
     if verbose:
-        print('{}.l10nrestore poistetaan'.format(l10nfile))
-    os.remove(l10nfile + '.l10nrestore')
+        print('{}.restore poistetaan'.format(filename))
+    os.remove(filename + '.restore')
 
-def init(args):
-    global short_opts, long_opts
-    steps = 1
-    verbose = False
-    opts, args = getopt.getopt(sys.argv[1:], short_opts, long_opts)
-    for opt, val in opts:
-        if opt in ('-l', '--l10ndir'):
-            l10n = val
-        if opt in ('-s', '--steps'):
-            steps = int(val)
-        if opt in ('-v', '--verbose'):
-            verbose = True
-        elif opt == '--help':
-            usage()
-            sys.exit(0)
-    if not args:
-        raise ProgramError
-    return args, l10n, steps, verbose
+## MAIN
 
-l10ndir = '.'
 try:
-    args, l10ndir, steps, verbose = init(sys.argv[1:])
+    opts, args = getopt.getopt(sys.argv[1:], short_opts, long_opts)
 except getopt.GetoptError:
-    die('Virhe komentorivillä (kokeile "--help")')
-except ValueError:
-    die('Virheellinen -s, --steps -arvo')
-except ProgramError:
-    die('Käyttö: l10nrestore [valitsimet] tsto... (kokeile "--help")')
-if not os.path.isdir(l10n):
-    die('Kansiota ei ole, lopetetaan: "{}"'.format(l10ndir))
+    die('virhe komentorivillä (kokeile "--help")')
+for opt, val in opts:
+    if opt in ('-l', '--l10ndir'):
+        l10ndir = val
+    if opt in ('-s', '--steps'):
+        try:
+            steps = int(val)
+        except ValueError:
+            die('virheellinen parametri "{}"'.format(val))
+    if opt in ('-v', '--verbose'):
+        verbose = True
+    elif opt == '--help':
+        usage()
+        sys.exit(0)
+if not args:
+    die('ei parametreja (kokeile "-help")')
+if not os.path.isdir(l10ndir):
+    die('kansio puuttuu: "{}"'.format(l10ndir))
 for arg in args:
     if not os.path.exists(os.path.join(l10ndir, arg)):
         warn('Tiedostoa ei ole, ohitetaan: "{}"'.format(arg))
         continue
+    filename = os.path.join(l10ndir, arg)
     try:
-        l10n_restore(l10ndir, arg, steps, verbose)
-    except (FileNotFoundError, PermissionError, IOError):
-        die('palautus ei onnistu: {}'.format(arg))
-    except ProgramError:
-        die('ei voi palauttaa niin montaa sukupolvea')
+        restore(filename, steps, verbose)
+    except FileNotFoundError:
+        die('tiedostoa ei löydy: {}'.format(filename))
+    except PermissionError:
+        die('ei käyttöoikeuksia: {}'.format(filename))
+    except IOError:
+        die('muu I/O-virhe: {}'.format(filename))
+    except ValueError as exc:
+        name, gens = exc.args
+        noun = 'sukupolvea' if gens > 1 else 'sukupolvi'
+        die('tiedostosta "{}" on vain {} palautus{}'.format(name, gens, noun))
